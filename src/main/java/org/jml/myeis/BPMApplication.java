705fd77c42c12311c0b2baaabb269eab9dfe1cd0
jeremiah.lumontod@gmail.com <jeremiah.lumontod@gmail.com>
2017-05-07 04:03:10 +0400

    disabling spring boot security, work in progress

--- src/main/java/org/jml/myeis/BPMApplication.java
+++ src/main/java/org/jml/myeis/BPMApplication.java
@@ -1,6 +1,7 @@
 package org.jml.myeis;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,6 +10,10 @@ import org.springframework.context.ApplicationContext;
 
 @EntityScan(basePackages = {"org.jml.myeis.domain"})
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class
+})
+
 public class BPMApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/jml/myeis/SecurityConfig.java
@@ -0,0 +1,29 @@
+package org.jml.myeis;
+
+import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(ManagementServerProperties.ACCESS_OVERRIDE_ORDER)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic()
+                .and()
+                .authorizeRequests().antMatchers("/","/springmvc/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().permitAll();
+
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+    }
+
+}
