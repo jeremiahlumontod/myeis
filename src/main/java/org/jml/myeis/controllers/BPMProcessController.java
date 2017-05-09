70bdedae971ff53a8e506ec4fe59c75344b17331
jeremiah.lumontod@gmail.com <jeremiah.lumontod@gmail.com>
2017-05-09 05:09:05 +0400

    request based process start up and task completion

--- src/main/java/org/jml/myeis/controllers/BPMProcessController.java
+++ src/main/java/org/jml/myeis/controllers/BPMProcessController.java
@@ -1,21 +1,21 @@
 package org.jml.myeis.controllers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import org.activiti.engine.HistoryService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.jml.myeis.domain.AjaxResponseBody;
 import org.jml.myeis.domain.Customer;
 import org.jml.myeis.domain.Product;
 import org.jml.myeis.domain.User;
 import org.jml.myeis.repository.BPMRepository;
 import org.jml.myeis.services.CustomerService;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
@@ -49,21 +49,70 @@ public class BPMProcessController {
     private RepositoryService repositoryService;
 
     @RequestMapping(value = "/bpm/workflowprocess/start", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<?> workflowProcessStart(@RequestBody Map<String, Object> payload) {
+    public ResponseEntity<?> workflowProcessStart(@RequestBody HashMap<String, Object> payload) {
         System.out.println("payload: " + payload);
+
+        Set keys = payload.keySet();
+        String bpmid = "";
+        for (Iterator i = keys.iterator(); i.hasNext(); ) {
+            String key = (String) i.next();
+            String value = (String) payload.get(key);
+            bpmid = key.equalsIgnoreCase("bpmid")?value:"";
+            System.out.println("key: " + key + ", value: " + value);
+        }
+
     	System.out.println("step 1. create instance of the process");
         Map<String, Object> variables = new HashMap<String, Object>();
         variables.put("newstring", new String());
         //all bpm process files should be place in src/main/resources/processes
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("accountant_Fixed_Asset_Disposal", variables);
+        //ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("accountant_Fixed_Asset_Disposal", variables);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(bpmid, variables);
         String procID = processInstance.getId();
+
+        Task task = taskService.createTaskQuery().processInstanceId(procID).singleResult();
+
         System.out.println("processInstance.getId(): " + processInstance.getId());
-        User user = new User("username","password","email");
-        AjaxResponseBody result = new AjaxResponseBody();
-        List<User> users = new ArrayList<>();
-        users.add(user);
-        result.setMsg("success");
-        result.setResult(users);
+        String result = "<result><processinstanceid>" + processInstance.getId() + "</processinstanceid><taskid>" + task.getId() + "</taskid></result>";
+        JSONObject obj = XML.toJSONObject(result);
+        result = obj.toString();
+        //processInstance = runtimeService.startProcessInstanceById(procID, variables);
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping(value = "/bpm/workflowprocess/task/finish", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<?> workflowProcessTaskFinish(@RequestBody HashMap<String, Object> payload) {
+        System.out.println("payload: " + payload);
+
+        Set keys = payload.keySet();
+        String bpmid = "",taskid="";
+        for (Iterator i = keys.iterator(); i.hasNext(); ) {
+            String key = (String) i.next();
+            String value = (String) payload.get(key);
+            if(key.equalsIgnoreCase("bpmidinstance")) {
+                bpmid = value;
+            }
+            if(key.equalsIgnoreCase("bpmidtask")) {
+                taskid  = value;
+            }
+            System.out.println("key: " + key + ", value: " + value);
+        }
+
+        System.out.println("step 1. create instance of the process");
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("newstring", new String());
+        //all bpm process files should be place in src/main/resources/processes
+        //ProcessInstance processInstance = runtimeService.startProcessInstanceById(bpmid, variables);
+
+        //String procID = processInstance.getId();
+
+        Task task = taskService.createTaskQuery().taskId(taskid).processInstanceId(bpmid).singleResult();
+        taskService.complete(task.getId(), verifyYNflag(task));
+        task = taskService.createTaskQuery().processInstanceId(bpmid).singleResult();
+
+        System.out.println("processInstance.getId(): " + bpmid);
+        String result = "<result><processinstanceid>" + bpmid + "</processinstanceid><nexttaskid>" + task.getId() + "</nexttaskid> <nexttaskname>" + task.getName() + "</nexttaskname></result>";
+        JSONObject obj = XML.toJSONObject(result);
+        result = obj.toString();
         return ResponseEntity.ok(result);
     }
 
@@ -77,5 +126,41 @@ public class BPMProcessController {
         //return "/process/startprocess";
     	return "bpm/startprocess";
     }
-    
+
+    @RequestMapping("/bpm/finishprocesstask")
+    public String processFinishTask() {
+        return "bpm/finishprocesstask";
+    }
+
+
+    public int getRandom() {
+        Random rand = new Random();
+        int  n = rand.nextInt(50) + 1;
+        //int  n = 5;
+        System.out.println("getRandom is: " + n);
+        return n;
+    }
+
+    public boolean yn(int n) {
+        return n%2==0;
+    }
+
+    public HashMap verifyYNflag(Task task) {
+        String abflag[] = task.getName().split("\\|");
+        HashMap variables = new HashMap<String, Object>();
+        if(abflag.length>2) {
+            //we got ourselves an exclusiveGateway. simulate a user decision of yes or no
+            //by generating a random decision
+            boolean yn = yn(getRandom());
+            String randomDecision = yn?"Yes":"No";
+            System.out.println("random decision is: " + randomDecision);
+            variables.put("bflag", yn);
+        }else{
+            variables = new HashMap<String, Object>();
+            variables.put("bflag", false);
+            System.out.println("random decision is: No");
+        }
+        return variables;
+    }
+
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -15,24 +15,29 @@ $(document).ready(function() {
         return false;
     });
 
+    $('#btnFinishTask').click(function() {
+        //alert("btnStart fired!");
+        finish_the_workflow_task();
+        return false;
+    });
+
 });
 
 
 function start_the_workflow_process() {
-    alert($("#username").val());
-    var search = {}
-    search["username"] = $("#username").val();
-    //search["email"] = $("#email").val();
+    //alert($("#username").val());
+    var bpmdata = {}
+    bpmdata["bpmid"] = $("#bpmid").val();
 
     $("#btnStart").prop("disabled", true);
-    var sdata = JSON.stringify(search);
-    alert("sdata: " + sdata);
+    var sdata = JSON.stringify(bpmdata);
+    //alert("bpmdata: " + bpmdata);
 
     $.ajax({
         type: "POST",
         contentType: "application/json",
         url: "http://localhost:8080/springmvc/bpm/workflowprocess/start",
-        data: JSON.stringify(search),
+        data: JSON.stringify(bpmdata),
         dataType: 'json',
         cache: false,
         timeout: 600000,
@@ -47,7 +52,7 @@ function start_the_workflow_process() {
 
         },
         error: function (e) {
-            alert("error! " + e.toString());
+            //alert("error! " + e.toString());
 
             var json = "<h4>Ajax Response</h4><pre>"
                 + e.responseText + "</pre>";
@@ -56,6 +61,52 @@ function start_the_workflow_process() {
 
             console.log("ERROR : ", e);
             $("#btnStart").prop("disabled", false);
+
+        }
+    });
+
+}
+
+
+function finish_the_workflow_task() {
+    //alert($("#username").val());
+    var bpmdata = {}
+    bpmdata["bpmidinstance"] = $("#bpmidinstance").val();
+    bpmdata["bpmidtask"] = $("#bpmidtask").val();
+    //search["email"] = $("#email").val();
+
+    $("#btnFinishTask").prop("disabled", true);
+    var sdata = JSON.stringify(bpmdata);
+    alert("bpmdata: " + sdata);
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "http://localhost:8080/springmvc/bpm/workflowprocess/task/finish",
+        data: JSON.stringify(bpmdata),
+        dataType: 'json',
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+            //alert("success! " + data);
+            var json = "<h4>Ajax Response</h4><pre>"
+                + JSON.stringify(data, null, 4) + "</pre>";
+            $('#feedback').html(json);
+
+            console.log("SUCCESS : ", data);
+            $("#btnFinishTask").prop("disabled", false);
+
+        },
+        error: function (e) {
+            alert("error! " + e.toString());
+
+            var json = "<h4>Ajax Response</h4><pre>"
+                + e.responseText + "</pre>";
+
+            $('#feedback').html(json);
+
+            console.log("ERROR : ", e);
+            $("#btnFinishTask").prop("disabled", false);
 
         }
     });
