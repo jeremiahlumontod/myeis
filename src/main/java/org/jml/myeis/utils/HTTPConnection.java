5bb92c498a29f0536a9c73bf6467044b73d9b48d
jeremiah.lumontod@gmail.com <jeremiah.lumontod@gmail.com>
2017-05-22 22:49:08 +0400

    added ssl and non ssl call test

--- src/main/java/org/jml/myeis/utils/HTTPConnection.java
+++ src/main/java/org/jml/myeis/utils/HTTPConnection.java
@@ -1,15 +1,17 @@
 package org.jml.myeis.utils;
 
+import com.google.gson.JsonObject;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.KeyStore;
 
-import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.*;
+
 public class HTTPConnection {
 
 
@@ -19,8 +21,13 @@ public class HTTPConnection {
     public static void main(String[] args) throws Exception {
 //        HTTPConnection.sendGet();
 //        HTTPConnection.getHttpResponse("https://localhost:8443/ssomyeis/oauth/token?grant_type=password&username=user&password=user","user","user");
-        HTTPConnection.getHttpResponse("http://localhost:8080/ssomyeis/oauth/token?grant_type=password&username=user&password=user","jml-client-id","12345");
+//        HTTPConnection.getHttpResponse("http://localhost:8080/ssomyeis/oauth/token?grant_type=password&username=user&password=user","jml-client-id","12345");
+//        HTTPConnection.getHttpResponseSSL("https://localhost:8443/ssomyeis/oauth/token?grant_type=password&username=user&password=user","jml-client-id","12345");
 //        HTTPConnection.getHttpResponse("http://localhost:8080/ssomyeis/oauth/token","user","user");
+
+
+//        new HTTPConnection().getHttpResponseSSL("https://localhost:8443/ssomyeis/oauth/token?grant_type=password&username=user&password=user","jml-client-id","12345");
+        new HTTPConnection().getHttpResponse("http://localhost:8080/ssomyeis/oauth/token?grant_type=password&username=user&password=user","jml-client-id","12345");
     }
 
     // HTTP GET request
@@ -98,7 +105,7 @@ public class HTTPConnection {
 //
 
 
-    public static String getHttpResponse(String address, String username, String password) throws Exception {
+    public String getHttpResponse(String address, String username, String password) throws Exception {
         URL url = new URL(address);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setConnectTimeout(30000); // 30 seconds time out
@@ -117,8 +124,80 @@ public class HTTPConnection {
             sb.append(line);
         }
         System.out.println("response: " + sb.toString());
+        JSONObject json = new JSONObject(sb.toString());
+        String access_token = (String) json.get("access_token");
+        String refresh_token = (String) json.get("refresh_token");
+        System.out.println("access_token: " + access_token + ", refresh_token: " + refresh_token);
         input.close();
         return sb.toString();
     }
+
+    public String getHttpResponseSSL(String address, String username, String password) throws Exception {
+
+        HostnameVerifier nullHostNameVerifier = new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+
+        //any client app that has this ssl info will be allowed to communicate
+        HttpsURLConnection.setDefaultHostnameVerifier(nullHostNameVerifier);
+
+        // Load the self-signed server certificate
+        char[] passphrase = "123456".toCharArray();
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+//        FileInputStream in = new FileInputStream("/tomcatclient.jks");
+        InputStream is  = this.getClass().getClassLoader().getResourceAsStream("tomcatclient.jks");
+        keyStore.load(is, passphrase);
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(keyStore, passphrase);
+        KeyManager[] keyManagers = kmf.getKeyManagers();
+        //tomcatclienttruststore.jks
+
+        KeyStore trustStore = KeyStore.getInstance("JKS");
+        ClassLoader classLoader = getClass().getClassLoader();
+//        in = new FileInputStream("tomcatclienttruststore.jks");
+        is  = this.getClass().getClassLoader().getResourceAsStream("tomcatclienttruststore.jks");
+        trustStore.load(is, passphrase);
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        tmf.init(trustStore);
+        TrustManager[] trustManagers = tmf.getTrustManagers();
+
+        // Create a SSLContext with the certificate
+        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+        //sslContext.init(null, new X509TrustManager[]{new NullX509TrustManager()}, new SecureRandom());
+        sslContext.init(keyManagers, trustManagers, null);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+
+
+        URL url = new URL(address);
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        conn.setConnectTimeout(30000); // 30 seconds time out
+
+        conn.setSSLSocketFactory(sslContext.getSocketFactory());
+
+        if (username != null && password != null){
+            String user_pass = username + ":" + password;
+            String encoded = Base64.encodeBase64String( user_pass.getBytes() );
+            conn.setRequestProperty("Authorization", "Basic " + encoded);
+        }
+        conn.setRequestMethod("POST");
+        String line = "";
+        StringBuffer sb = new StringBuffer();
+        BufferedReader input = new BufferedReader(new InputStreamReader(conn.getInputStream()) );
+//        System.out.println("response: " + input.toString());
+        while((line = input.readLine()) != null) {
+            sb.append(line);
+        }
+        System.out.println("response: " + sb.toString());
+        JSONObject json = new JSONObject(sb.toString());
+        String access_token = (String) json.get("access_token");
+        String refresh_token = (String) json.get("refresh_token");
+        System.out.println("access_token: " + access_token + ", refresh_token: " + refresh_token);
+        input.close();
+        return sb.toString();
+    }
+
 }
 
