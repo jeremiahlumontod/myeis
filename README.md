eaad6637b768f2932141f381693d6cbfd6630812
jeremiah.lumontod@gmail.com <jeremiah.lumontod@gmail.com>
2017-05-30 15:23:22 +0400

    testing spring boot filters

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # myeis
+
+1) start the process
+http://localhost:8080/springmvc/bpm/startprocess
+
+{
+    "result": {
+        "firsttaskid": "280006:1 | Facilities Manager",
+        "processinstanceid": 280001,
+        "processdetailsid": 891
+    }
+}
+
+2) input first task id, process instance id, process details id to the following url:
+http://localhost:8080/springmvc/bpm/finishprocesstask
--- src/main/java/org/jml/myeis/filters/SecurityFilter.java
+++ src/main/java/org/jml/myeis/filters/SecurityFilter.java
@@ -1,7 +1,40 @@
 package org.jml.myeis.filters;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
- * Created by jeremiahlumontod on 5/30/17.
+ * @author ekansh
  */
-public class SecurityFilter {
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SecurityFilter implements Filter {
+
+    private static final boolean CONDITION = true;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("SecurityFilter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("SecurityFilter doFilter");
+        if(CONDITION==true)
+            chain.doFilter(request,response);
+        else{
+            ((HttpServletResponse)response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("SecurityFilter destroy");
+    }
 }
